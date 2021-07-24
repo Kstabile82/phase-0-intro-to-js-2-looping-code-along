2ebf0b3cf5962bae8541fdec40afcbad4c092ad7
KarinaStabile <karina.stabile@gmail.com>
2021-07-24 19:57:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards (names) {
+    let returnArr = []; 
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`; 
+        returnArr.push(message); 
+    }
+    console.log(returnArr); 
+    debugger;
+}
+writeCards([ 'Lisa', 'Kaitlin', 'Jan' ]); 
+
+  function countDown (num) {
+    while (num >= 0) {
+        console.log(num); 
+        debugger;
+        num--; 
+    }
+}
+countDown(10); 
+
